688e173bb11166269733f4c906e17551c8202a77
23ib29_zima <23ib29_zima@U1-04>
2025-09-30 15:13:40 +0200

    Přidejte soubory projektu.

---
+++ WPFToDoApp/Database/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPFToDoApp.Models;
+
+namespace WPFToDoApp.Database
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDoEntity> ToDoSet { get; set; }
+
+        public ToDoContext() 
+        { 
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source = ToDoAppDb.db");
+        }
+    }
+}
---
+++ WPFToDoApp/Models/ToDoEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace WPFToDoApp.Models
+{
+    public class ToDoEntity : INotifyPropertyChanged
+    {
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsDone)));
+            }
+        }
+
+        public readonly string title;
+
+        private bool _isDone;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ToDoEntity(string title, bool isDone = false)
+        {
+            _isDone = isDone;
+            this.title = title;
+        }
+
+        public override string ToString() => title;
+    }
+}
